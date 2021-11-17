a5ae12173c60f823a4a8d4fb4cbf1fb4b2261624
Rahat Tahmid <rahat.tahmid@gmail.com>
2021-11-17 10:10:25 +0600

    new added java file

---
+++ app/src/main/java/com/example/multiple_screens_appjava/Assignment.java
@@ -0,0 +1,24 @@
+package com.example.multiple_screens_appjava;
+
+public class Assignment {
+
+    int a,b;
+    public Assignment() {
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
